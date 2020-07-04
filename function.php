c02c90ef686b6f051fd985f43d4930bc33811157
sixiez <67650021+sixiez@users.noreply.github.com>
2020-07-04 12:44:38 +0700

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.52.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.726970864864974,115.17595309764147";
+$header[] = "X-Location: -6.9170289,107.6098005";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
